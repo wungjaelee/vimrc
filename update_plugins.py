33a6424d16c9ed0bcfde1dcead1dfa476caf3842
Wung Jae Lee <wunglee2021@u.northwestern.edu>
2021-05-18 02:12:40 +0900

    moved vimwiki and vim-startify from my_plugins to sources_non_forked

--- update_plugins.py
+++ update_plugins.py
@@ -58,6 +58,8 @@ typescript-vim https://github.com/leafgarland/typescript-vim
 vim-javascript https://github.com/pangloss/vim-javascript
 vim-python-pep8-indent https://github.com/Vimjas/vim-python-pep8-indent
 mru.vim https://github.com/vim-scripts/mru.vim
+vimwiki https://github.com/vimwiki/vimwiki
+vim-startify https://github.com/mhinz/vim-startify
 """.strip()
 
 GITHUB_ZIP = "%s/archive/master.zip"
